bf7e5c75939e5a39d65fdf22653dbb91d8998aef
A$[GREYWOLF] <asschool@yandex.ru>
2015-02-11 21:35:58 +0500

    Revert "Fixes"
    This reverts commit f137aadb44367d5c2de9b3e44b299d9d348e9cea.
    
    Conflicts:
    	src/client/components/Graphics.cpp
    	src/client/components/Graphics.h
    	src/client/components/graphics/PlayerModel.cpp

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ -o teeworlds3d -O3 -DLINUX --std=c++11 src/client/*.cpp src/client/components/*.cpp src/client/components/graphics/*.cpp -lGL -lGLEW -lSDL2 -lSDL2_image -lSDL2_ttf -lGLU
+g++ -o teeworlds3d -O3 -DLINUX --std=c++11 src/client/*.cpp src/client/components/*.cpp src/client/components/graphics/*.cpp -lGL -lGLEW -lSDL2 -lSDL2_image -lSDL_ttf -lGLU
